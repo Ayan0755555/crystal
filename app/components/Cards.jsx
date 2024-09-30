34cbb0ec6b5e2388fcc29022074af384bbdc2e13
ayan <mohdayanrehman07@gmail.com>
2024-09-30 11:40:17 +0530

    solved

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { ChevronDownIcon } from "lucide-react";
 import Image from "next/image";
 import CardsIma from "../fonts/image.png";
-import "../../module/cards.css";
 
 const products = [
   {
